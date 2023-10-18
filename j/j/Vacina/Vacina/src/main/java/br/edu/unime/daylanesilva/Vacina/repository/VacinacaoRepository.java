82591e32b92e4ad409eeb0e288b273c61c1620cd
Daylane Silva <69282025+daylane@users.noreply.github.com>
2023-10-18 20:56:51 -0300

    deletado paciente

--- j/j/Vacina/Vacina/src/main/java/br/edu/unime/daylanesilva/Vacina/repository/VacinacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.unime.daylanesilva.Vacina.repository;
-
-import br.edu.unime.daylanesilva.Vacina.entity.Vacinacao;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface VacinacaoRepository extends MongoRepository<Vacinacao, String> {
-}
