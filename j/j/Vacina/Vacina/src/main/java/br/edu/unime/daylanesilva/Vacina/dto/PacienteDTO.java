1299b42de31b8774d49d426424f7403c6b4436c1
Daylane Silva <69282025+daylane@users.noreply.github.com>
2023-11-18 19:31:08 -0300

    delete

--- j/j/Vacina/Vacina/src/main/java/br/edu/unime/daylanesilva/Vacina/dto/PacienteDTO.java
+++
@@ -1,9 +0,0 @@
-package br.edu.unime.daylanesilva.Vacina.dto;
-
-public class PacienteDTO {
-    public String nome;
-    public int idade;
-    public String bairro;
-    public String municipio;
-    public String estado;
-}
--- j/j/Vacina/Vacina/src/main/java/br/edu/unime/daylanesilva/Vacina/exception/VacinaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.unime.daylanesilva.Vacina.exception;
-
-public class VacinaNotFoundException extends RuntimeException{
-    public VacinaNotFoundException(String message){
-        super(message);
-        }
-}
